a97ea0b55bd6e361c66b5f53f25f1bf0e04df893
Nelsonmwaura <Nelsontoby5@gmail.com>
2020-08-25 21:51:10 +0300

    creating qoute-form

--- Qoutes/src/app/app.module.ts
+++ Qoutes/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QouteComponent } from './qoute/qoute.component';
 import { QouteDetailComponent } from './qoute-detail/qoute-detail.component';
+import { QouteFormComponent } from './qoute-form/qoute-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QouteComponent,
-    QouteDetailComponent
+    QouteDetailComponent,
+    QouteFormComponent
   ],
   imports: [
     BrowserModule,
